bf32ec0f52806fd6c0f49b05a2623f262c21e523
ragaskar <ragaskar@gmail.com>
2012-09-01 13:58:35 -0700

    Add Rails, Jasmine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WitchDoctor::Application.config.secret_token = '3371f15d8898ea59fc4ce2f40cdfa51697694f3ab0a670f75f059aeb1cdca32256c3b68c7ccfa30115e4c98a3025682b98c55d12c13d7876ace175265cc9bfa8'
