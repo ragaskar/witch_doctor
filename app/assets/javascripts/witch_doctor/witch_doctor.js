3423da05fe238574ad07679c1349daf3bed258ce
ragaskar <ragaskar@gmail.com>
2012-09-02 08:38:04 -0700

    WIP

--- app/assets/javascripts/witch_doctor/witch_doctor.js
+++ app/assets/javascripts/witch_doctor/witch_doctor.js
@@ -1,5 +1,28 @@
 var WitchDoctor;
 (function($) {
-  WitchDoctor = function WitchDoctor() {
+  var stageRight,
+  stageCenter,
+  stageLeft;
+  WitchDoctor = function(options) {
+    stageRight = $('<div>'),
+    stageCenter = $('<div>'),
+    stageLeft = $('<div>');
+    options.el.append(stageLeft).
+      append(stageCenter).
+      append(stageRight);
+    var self = {
+      show: function(el) {
+        stageCenter.html(el);
+      },
+      transition: function(options) {
+        if (options.direction === 'fromRight') {
+          stageRight.html(options.el);
+        }
+      },
+      centerStage: function() {
+        return stageCenter;
+      }
+    }
+    return self;
   };
 }(jQuery || Zepto));
